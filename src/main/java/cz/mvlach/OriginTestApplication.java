d5fa06fa139b42dd43a6a20fa0c77c50f1d92bbd
Miloslav Vlach <miloslav.vlach@gmail.com>
2016-07-01 00:26:35 +0200

    initial

--- src/main/java/cz/mvlach/OriginTestApplication.java
+++ src/main/java/cz/mvlach/OriginTestApplication.java
@@ -1,25 +1,12 @@
 package cz.mvlach;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
 @SpringBootApplication
-public class OriginTestApplication extends SpringBootServletInitializer {
+public class OriginTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OriginTestApplication.class, args);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(OriginTestApplication.class);
 	}
 }
